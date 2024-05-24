1764bb9be6097ee297748f4beff1edecee581dfc
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-05-24 21:19:32 +0100

    second page

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -8,31 +8,44 @@ import { Button } from "@/components/ui/button";
 import user from  "../../components/image/image.png"
 export default function Home() {
 	return (
-		<main className="p-2 flex flex-col items-center bg-cover bg-center min-h-screen w-full lg:mt-4">
+		<main className="p-2 flex flex-col items-center bg-cover bg-center min-h-screen w-full lg:mt-0">
 			<Nav />
 			<section className="intro flex flex-col items-center text-center mx-4 lg:mx-0 mt-20 lg:mt-14">
 				<div className="waitlist mt-8 lg:mt-6 bg-[#161616] text-[#F1F7FEB5] rounded-2xl pt-1 pb-1 pl-3 pr-3 flex items-center space-x-3 relative border border-[#4E4E4E]">
-					<p className="text-[#F1F7FEB5] text-xs lg:text-sm">
+					<p className=" date text-[#F1F7FEB5] text-xs lg:text-sm">
 						April 11th, 2024
 					</p>
 				</div>
 
 				<div className="text mt-5 text-white">
-					<h1 className="text-lg lg:text-5xl">What's next after 8,000 users</h1>
-					<p className="about mt-3 lg:mt-5 text-base lg:text-lg text-[#F1F7FEB5]">
+					<h1
+						className="text-[#D9D9D9] text-lg lg:text-5xl font-display font-medium leading-[64px] lg:leading-[64px] tracking-[1.4px]"
+						style={{
+							fontFamily: "Inter Display, sans-serif",
+							fontStyle: "normal",
+							fontWeight: 500,
+							letterSpacing: "-1.4px",
+						}}
+					>
+						What's next after 8,000 users
+					</h1>
+					<p className="about mt-3 lg:mt-0 text-base lg:text-lg text-[#F1F7FEB5]">
 						How we got here and where we're going next.
 					</p>
 				</div>
 
-				<div className="waitlist mt-3 bg-transparent text-[#F1F7FEB5] rounded-2xl -pt-1 -pb-1 pl-0 pr-2 mt- flex items-center space-x-3 relative border border-[#4E4E4E]">
+				<div
+					className="waitlist mt-3 bg-transparent text-[#F1F7FEB5] rounded-[90px] pt-0 pb-0 pl-0 pr-2 flex items-center space-x-3 relative border"
+					style={{ borderColor: "#494949" }}
+				>
 					<div className="rounded-full overflow-hidden">
-						<Image src={user} alt="user" width={40} height={40} />
+						<Image src={user} alt="user" width={30} height={30} />
 					</div>
 					<p className="text-[#F1F7FEB5] text-xs lg:text-sm">Nuel Adeleke</p>
 				</div>
 
 				<div className="text text-[#F1F7FEB5] text-left max-w-[799px]">
-					<div>
+					<div className="paragraph1 flex flex-col items-start ">
 						<p className="mt-4 text-xs lg:text-base">
 							Since launching publicly ten months ago, we crossed 100,000
 							verified users on Resend. Our open source project is already at{" "}
@@ -49,16 +62,18 @@ export default function Home() {
 							Here's a quick recap of our journey so far and what's next.
 						</p>
 					</div>
-					<div>
-						<h3 className="font-bold text-lg lg:text-xl mt-6 text-white">
+
+					<div className="paragraph2">
+						<h3 className="font-semibold text-lg lg:text-xl mt-6 text-white">
 							How we got here
 						</h3>
-						<p className="mt- text-xs lg:text-sm">
+						<p className="text-[#F1F7FEB5]  leading-8 mt-2 lg:text-sm">
 							Every idea has a story, and ours started in August 2022.
 						</p>
 					</div>
+
 					<div className="text text-[#F1F7FEB5] text-left max-w-[799px]">
-						<h3 className="font-bold text-lg lg:text-xl mt-6  text-white">
+						<h3 className="font-semibold text-lg lg:text-xl mt-6  text-white">
 							Aug 2022: Reimagining email
 						</h3>
 						<p className="mt-4 text-xs lg:text-base">
