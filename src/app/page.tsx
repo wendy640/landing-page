8677c864cf7c73c0a89ea5ec68e8a2ce5beffc25
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-05-24 20:38:27 +0100

    responsivness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+"use client"
 import { useState } from "react";
 import Nav from "@/components/nav";
 import { Input } from "@/components/ui/input";
 import { ChevronRight, Mail } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import "./globals.css";
 
 export default function Home() {
 	const [email, setEmail] = useState("");
@@ -21,26 +22,52 @@ export default function Home() {
 	};
 
 	return (
-		<main className="p-2 flex flex-col items-center bg-cover bg-center min-h-screen w-full mt-40 lg:mt-0 ">
+		<main className="p-2 flex flex-col items-center bg-cover bg-center min-h-screen w-full mt-40 lg:mt-0">
 			<Nav />
-			<section className="intro flex flex-col items-center text-center mx-6  lg:mx-0 -mt-10 lg:mt-0">
-				<div className="waitlist mt-6 lg:mt-40 bg-[#161616] text-[#F1F7FEB5] rounded-2xl pt-1 pb-1 pl-3 pr-3 flex items-center space-x-3 relative border border-[#4E4E4E]">
+			<section className="intro flex flex-col items-center text-center mx-6 lg:mx-0 -mt-10 lg:-mt-6">
+				<div className="waitlist mt-6 lg:mt-40 bg-[#161616] text-[#F1F7FEB5] rounded-full p-1 pl-3 pr-3 flex items-center space-x-3 relative border border-[#4E4E4E]">
 					<p className="text-[#F1F7FEB5] text-sm">Waitlist v1</p>
 					<span className="text-xl font-bold px-2 absolute left-1/2 transform -translate-x-9 -translate-y-1">
 						.
 					</span>
 					<p className="text-[#F1F7FEB5] text-sm">Coming Soon</p>
 				</div>
-
 				<div className="text mt-10 text-white">
-					<h1 className="text-4xl lg:text-7xl">
+					<h1
+						className="text-4xl lg:text-7xl"
+						style={{
+							textAlign: "center",
+							textShadow:
+								"-1px -1px 0px rgba(255, 255, 255, 0.20), 1px 1px 0px rgba(0, 0, 0, 0.10)",
+							WebkitTextStrokeWidth: "0.4px",
+							WebkitTextStrokeColor: "rgba(255, 255, 255, 0.50)",
+							fontFamily: "Inter Display, sans-serif",
+							fontWeight: "500",
+							lineHeight: "80px",
+							letterSpacing: "-1.8px",
+							background:
+								"linear-gradient(135deg, #FFF 30%, rgba(255, 255, 255, 0.50) 100%)",
+							WebkitBackgroundClip: "text",
+							WebkitTextFillColor: "transparent",
+						}}
+					>
 						Your paycheck but
 						<span className="hidden lg:inline">
 							<br />
 						</span>{" "}
 						with super powers
 					</h1>
-					<p className="about mt-8 text-base lg:text-lg text-[#F1F7FEB5]">
+					<p
+						className="about mt-8"
+						style={{
+							color: "rgba(241, 247, 254, 0.71)",
+							textAlign: "center",
+							fontFamily: "Inter Display, sans-serif",
+							fontSize: "18px",
+							fontWeight: "400",
+							lineHeight: "24px",
+						}}
+					>
 						Wagestack is a programmable salary wallet designed for employees
 						<span className="lg:inline hidden">
 							<br />
@@ -48,13 +75,12 @@ export default function Home() {
 						and integrates well with your favorite payroll platforms.
 					</p>
 				</div>
-
-				<div className="contacts flex items-center space-x-2 mt-10 mb-3  lg:w-auto">
+				<div className="contacts flex items-center space-x-2 mt-10 mb-4 lg:w-auto">
 					<div className="relative w-72 md:w-72 lg:w-86">
 						<Input
 							type="email"
 							placeholder="Email"
-							className={`pl-10 bg-[#161616] border-[#4E4E4E] ${
+							className={`pl-10 bg-[#141413] border-[#4E4E4E] ${
 								isFocused ? "text-white" : "text-gray-400"
 							}`}
 							value={email}
@@ -65,6 +91,7 @@ export default function Home() {
 						<Mail className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
 					</div>
 				</div>
+
 				<div className="join mb-7 flex justify-center items-center w-72 md:w-72 lg:w-86">
 					<Button
 						variant="outline"
@@ -78,15 +105,33 @@ export default function Home() {
 				</div>
 			</section>
 
-			<div className="next mt-60 lg:mt-20 bg-[#161616] rounded-full p-1 flex items-center justify-center border border-gray border-[#4E4E4E]">
-				<p className="flex items-center text-[#F1F7FEB5] space-x-2 text-xs rounded-full ml-2">
+			<div className="next mt-40 lg:mt-10 bg-[#161616] rounded-full p-1 flex items-center justify-center border border-gray border-[#4E4E4E]">
+				<p
+					className="flex items-center text-[#F1F7FEB5] space-x-2 text-xs rounded-full ml-2"
+					style={{
+						display: "inline-flex",
+						padding: "7px 7px 7px 15px",
+						justifyContent: "center",
+						alignItems: "center",
+						gap: "2px",
+						borderRadius: "90px",
+						border: "1px solid rgba(78, 78, 78, 0.00)",
+						background: "#141413",
+						boxShadow: "0px 10px 12px 0px rgba(25, 25, 25, 0.40)",
+						color: "#CCCCCC",
+					}}
+				>
 					What’s next after 8000 users @wagestack
 					<Link href="/read">
 						<Button
 							size="sm"
-							className="read-more ml-5 bg-black rounded-full flex items-center space-x-1 px-2"
+							className="read-more ml-5 bg-black rounded-full flex items-center space-x-1 px-3"
+							style={{
+								color: "#FFFFFF",
+								background: "#000000",
+							}}
 						>
-							<span className="text-white">Read more</span>
+							<span className="text-white "style={{ fontSize: '12px' }}>Read more</span>
 							<ChevronRight className="h-3 w-3 text-white" />
 						</Button>
 					</Link>
